d890e6446286492af987e353ac37e54004035d73
Евгений Мишарев <misharevea@gmail.com>
2023-11-21 17:53:04 +0300

    Статистика запу

--- WallFinishCreatorOptions/WallFinishCreatorOptionsCommand.cs
+++ WallFinishCreatorOptions/WallFinishCreatorOptionsCommand.cs
@@ -1,5 +1,8 @@
 ﻿using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace WallFinishCreatorOptions
 {
@@ -8,6 +11,12 @@ namespace WallFinishCreatorOptions
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
+            try
+            {
+                GetPluginStartInfo();
+            }
+            catch { }
+
             WallFinishCreatorOptionsWPF wallFinishCreatorOptionsWPF = new WallFinishCreatorOptionsWPF();
             wallFinishCreatorOptionsWPF.ShowDialog();
             if (wallFinishCreatorOptionsWPF.DialogResult != true)
@@ -15,6 +24,27 @@ namespace WallFinishCreatorOptions
                 return Result.Cancelled;
             }
             return Result.Succeeded;
+        }
+        private static void GetPluginStartInfo()
+        {
+            // Получаем сборку, в которой выполняется текущий код
+            Assembly thisAssembly = Assembly.GetExecutingAssembly();
+            string assemblyName = "WallFinishCreatorOptions";
+            string assemblyNameRus = "Свойства отделки стен";
+            string assemblyFolderPath = Path.GetDirectoryName(thisAssembly.Location);
+
+            int lastBackslashIndex = assemblyFolderPath.LastIndexOf("\\");
+            string dllPath = assemblyFolderPath.Substring(0, lastBackslashIndex + 1) + "PluginInfoCollector\\PluginInfoCollector.dll";
+
+            Assembly assembly = Assembly.LoadFrom(dllPath);
+            Type type = assembly.GetType("PluginInfoCollector.InfoCollector");
+            var constructor = type.GetConstructor(new Type[] { typeof(string), typeof(string) });
+
+            if (type != null)
+            {
+                // Создание экземпляра класса
+                object instance = Activator.CreateInstance(type, new object[] { assemblyName, assemblyNameRus });
+            }
         }
     }
 }
